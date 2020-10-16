774e07c401114f23c4ed898c8b1c13a01323a76e
alisa-one <72848258+alisa-one@users.noreply.github.com>
2020-10-17 03:29:30 +0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите Ваше имя: ");
+        String name = in.nextLine();
+        System.out.println("Здравствуйте" + "  " + name);
+        in.close();
+    }
+}
+
